4096de3b9ade08e013ccaebd658dbbcf3bf634d3
RMusaH <rafaelmusah@gmail.com>
2023-03-20 00:06:18 +0100

    inicial tests

--- src/game/game.s
+++ src/game/game.s
@@ -24,28 +24,65 @@ along with gamelib-x64. If not, see <http://www.gnu.org/licenses/>.
 
 .section .game.data
 
+position:	.quad 0x0				#counter of position
+
 .section .game.text
+.equ		vgaStart, 0xB8000               #start of vga
+.equ    	vgaEnd, 0xB8FA0                 #end if vga
 
 gameInit:
+
+	movq    $39772, %rdi            #reloadValue for 60 Hz since this game's logic is frame-dependent
+    call    setTimer                #call setTimer to load it to 60 Hz
+
+	movq    $vgaStart, %rdi         #start of graphics memory
+
+	movq	$0, %r12
+	movq	$0, %r13
+
+initLoop: 
+    movw    $0, (%rdi)              #erase what was there before
+    addq    $2, %rdi                #get next memory address
+    cmpq    $vgaEnd, %rdi           #check if it is the end
+    jl      initLoop                #if its not the end continue
+
 	ret
 
 gameLoop:
+
+	incq	%r12
+
+	movq    $vgaStart, %rdi
+	movw	$0x0F3D, (%rdi)
+
 	# Check if a key has been pressed
 	call	readKeyCode
 	cmpq	$0, %rax
-	je		1f
-	# If so, print a 'Y'
-	movb	$'Y', %dl
-	jmp		2f
+	je		noClick
+	
+	
+	#movb	$'1', %dl
+	#movq	$1, %rdi
+	#movq	$0, %rsi
+	#movb	$0x0f, %cl
+	#call	putChar
 
-1:
-	# Otherwise, print a 'N'
-	movb	$'N', %dl
+noClick:
 
-2:
-	movq	$0, %rdi
-	movq	$0, %rsi
-	movb	$0x0f, %cl
-	call	putChar
+	#movb	$0, %dl
+	#movq	%r12, %r8
+	#decq	%r8
+	#movq	%r8, %rdi
+	#movq	$0, %rsi
+	#movb	$0x0f, %cl
+	#call	putChar
+
+	#movb	$'0', %dl
+	#movq	%r12, %rdi
+	#movq	$0, %rsi
+	#movb	$0x0f, %cl
+	#call	putChar
+
+endloop:
 
 	ret
